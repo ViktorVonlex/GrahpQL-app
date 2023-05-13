43511fa3ddae8f463a454f9e75c60396427834b7
Jiří Šuster <suster.ji@gmail.com>
2023-05-13 21:23:15 +0200

    PublisherByBookId in controller

---
+++ src/main/java/com/vonlex/be/controller/PublisherController.java
@@ -0,0 +1,25 @@
+package com.vonlex.be.controller;
+
+import com.vonlex.be.model.Publisher;
+import com.vonlex.be.repository.PublisherRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/publishers")
+public class PublisherController {
+
+    @Autowired
+    private PublisherRepository publisherRepository;
+
+
+    @GetMapping("/publisherByBookId/{bookId}")
+    public Publisher byAuthorId(@PathVariable Long bookId) {
+        return publisherRepository.findPublisherByBooksId(bookId);
+    }
+}
--- src/main/java/com/vonlex/be/repository/PublisherRepository.java
+++ src/main/java/com/vonlex/be/repository/PublisherRepository.java
@@ -4,4 +4,6 @@ import com.vonlex.be.model.Publisher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+
+    Publisher findPublisherByBooksId(Long id);
 }
